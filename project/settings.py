096d85283c34eb380e9cd795fcad6ea40bdab916
우태호 <{>
2025-05-11 01:01:30 +0900

    plz

--- project/settings.py
+++ project/settings.py
@@ -75,9 +75,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'project.urls'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ORIGINS = [
+    "http://localhost:3000",
+]
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 TEMPLATES = [
     {
