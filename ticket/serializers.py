7b7f868fde9716743c3bbd412c79e686da5177c8
우태호 <{>
2025-05-10 22:40:45 +0900

    ticket serializer  complete

---
+++ ticket/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Ticket, TicketPurchase
+
+class TicketSerializer(serializers.ModelSerializer):
+    performance = serializers.StringRelatedField()  # 공연 제목 표시용
+    class Meta:
+        model = Ticket
+        fields = ['id', 'performance', 'price', 'quantity', 'created_at']
+
+class TicketPurchaseSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    ticket = TicketSerializer(read_only=True)
+
+    class Meta:
+        model = TicketPurchase
+        fields = ['id', 'user', 'ticket', 'quantity', 'purchased_at']
